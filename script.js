3f3ab61b90656506ee076a8009d52ae640c7b427
Free Archer <bespalov.alexey@yandex.ru>
2019-02-27 17:32:05 +0700

    Делаю рекурсию

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
+const createDivBkContener = (bkTreeNodes, divContener) => {
+    for (bkTreeNodesChild of bkTreeNodes.children) {
+        if (bkTreeNodesChild.children) {
+            const divBKContener = document.createElement('div')
+            divBKContener.classList.add('bk-contener')
+            divBKContener.textContent = bkTreeNodesChild.title
+            createDivBkContener(bkTreeNodesChild, divBKContener)
+        } else {
+            const divBK = document.createElement('div')
+            divBK.classList.add('bk')
+            divBK.textContent = bkTreeNodesChild.title
+
+            divContener.appendChild(divBK)
+            return divContener
+        }
+    }
+
+}
+
 const getBookmarks = () => {
-    const bookmarkTreeNodes = chrome.bookmarks.getSubTree('1938', (arBookmarkTreeNodes) => {
-      console.log(arBookmarkTreeNodes);
+    chrome.bookmarks.getSubTree('1938', (startTreeNodes) => {
+
       const bkContener = document.getElementById('bookmarks');
 
-      for (const bkTreeNodes of arBookmarkTreeNodes) {
-          for (const childbkTreeNodes of bkTreeNodes.children) {
-              divBKContener = createDivBkContener(childbkTreeNodes);
+      //for (const bkTreeNodes of arBookmarkTreeNodes) {
+          //for (const childbkTreeNodes of startTreeNodes[0].children) {
+              divBKContener = createDivBkContener(startTreeNodes[0], bkContener);
 
-              bkContener.appendChild(divBKContener);
-          }
-      }
+              //bkContener.appendChild(divBKContener);
+          //}
+      //}
 
           //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
       })
     }
 
-const createDivBkContener = (bkTreeNodes) => {
-    const divBKContener = document.createElement('div')
-    divBKContener.classList.add('bk-contener')
 
-    const divBK = document.createElement('div')
-    divBK.classList.add('bk')
-    divBK.textContent = bkTreeNodes.title
-
-    divBKContener.appendChild(divBK)
-
-    return divBKContener
-}
 
 
 document.addEventListener('DOMContentLoaded', () => {
     getBookmarks();
 
-    chrome.bookmarks.search('ПЛИТКИ', (bookmarkTreeNodes) => {
-        console.log(bookmarkTreeNodes);
-    })
+    // chrome.bookmarks.search('ПЛИТКИ', (bookmarkTreeNodes) => {
+    //     console.log(bookmarkTreeNodes);
+    // })
   })
