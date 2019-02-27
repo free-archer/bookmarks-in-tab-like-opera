d92d407adb0b67f360dedee4fd3a44f45c6f8a6a
Free Archer <bespalov.alexey@yandex.ru>
2019-02-27 14:07:46 +0700

    Вывел верхий уровень

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const getBookmarks = () => {
-    const bookmarkTreeNodes = chrome.bookmarks.getSubTree('1938', (bookmarkTreeNodes) => {
-      console.log(bookmarkTreeNodes);
+    const bookmarkTreeNodes = chrome.bookmarks.getSubTree('1938', (arBookmarkTreeNodes) => {
+      console.log(arBookmarkTreeNodes);
+      const bkContener = document.getElementById('bookmarks');
+
+      for (const bkTreeNodes of arBookmarkTreeNodes) {
+          for (const childbkTreeNodes of bkTreeNodes.children) {
+              divBKContener = createDivBkContener(childbkTreeNodes);
+
+              bkContener.appendChild(divBKContener);
+          }
+      }
+
           //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
       })
-    };
-  
-  document.addEventListener('DOMContentLoaded', () => {
+    }
+
+const createDivBkContener = (bkTreeNodes) => {
+    const divBKContener = document.createElement('div')
+    divBKContener.classList.add('bk-contener')
+
+    const divBK = document.createElement('div')
+    divBK.classList.add('bk')
+    divBK.textContent = bkTreeNodes.title
+
+    divBKContener.appendChild(divBK)
+
+    return divBKContener
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
     getBookmarks();
 
     chrome.bookmarks.search('ПЛИТКИ', (bookmarkTreeNodes) => {
         console.log(bookmarkTreeNodes);
     })
-  });
+  })
