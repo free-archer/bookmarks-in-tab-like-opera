6459738445c1cb919aad1430271e035669ee6653
Free Archer <bespalov.alexey@yandex.ru>
2019-02-28 12:06:03 +0700

    Вывод сделан

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 const createDivBkContener = (bkTreeNodes, divContener) => {
     for (bkTreeNodesChild of bkTreeNodes.children) {
         if (bkTreeNodesChild.children) {
+            const contenerBkContener = document.createElement('div')
+            contenerBkContener.classList.add('contener-bk-contener')
+            divContener.appendChild(contenerBkContener)
+
             const divBKContener = document.createElement('div')
             divBKContener.classList.add('bk-contener')
-            divBKContener.textContent = bkTreeNodesChild.title
-            divContener.appendChild(divBKContener)
+            contenerBkContener.appendChild(divBKContener)
+
+            const span = document.createElement('span')
+            span.textContent= bkTreeNodesChild.title
+            contenerBkContener.appendChild(span)
 
             createDivBkContener(bkTreeNodesChild, divBKContener)
         } else {
-            const divBK = document.createElement('div')
-            divBK.classList.add('bk')
-            divBK.textContent = bkTreeNodesChild.title
 
-            divContener.appendChild(divBK)
+            if (divContener.classList[0] == 'contener') {
+                const divBKContener = document.createElement('div')
+                divBKContener.classList.add('bk-contener')
+                divContener.appendChild(divBKContener)
+                
+                const divBK = document.createElement('div')
+                divBK.classList.add('bk')
+                divBK.textContent = bkTreeNodesChild.title
+
+                divBKContener.appendChild(divBK)
+            } else {
+                const divBK = document.createElement('div')
+                divBK.classList.add('bk')
+                divBK.textContent = bkTreeNodesChild.title
+
+                divContener.appendChild(divBK)
+            }
+
+            // const aLink = document.createElement('a')
+            // aLink.setAttribute('href', bkTreeNodesChild.url)
+            // aLink.textContent= bkTreeNodesChild.title
+            // contenerBkContener.appendChild(aLink)
         }
     }
 
