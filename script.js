64b6f751c0f9077552d68c7ec6ae4c4970d5a921
Free Archer <bespalov.alexey@yandex.ru>
2019-02-28 17:54:49 +0700

    colors

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function rand(){
+    var c=parseInt(Math.random()*255).toString(16);
+    return (""+c).length==1?'0'+c:c;
+}    
+const randomColor = () => {
+        var c='#'+rand()+rand()+rand();
+        alert(c);
+ }
+
 const createDivBkContener = (bkTreeNodes, divContener) => {
     for (bkTreeNodesChild of bkTreeNodes.children) {
         if (bkTreeNodesChild.children) {
@@ -11,7 +20,7 @@ const createDivBkContener = (bkTreeNodes, divContener) => {
 
             const span = document.createElement('span')
             span.classList.add('title')
-            span.textContent= bkTreeNodesChild.title
+            span.textContent= bkTreeNodesChild.title.substring(0, 20)
             contenerBkContener.appendChild(span)
 
             createDivBkContener(bkTreeNodesChild, divBKContener)
@@ -19,6 +28,7 @@ const createDivBkContener = (bkTreeNodes, divContener) => {
             if (divContener.classList[0] == 'contener') {
                 const contenerBkContener = document.createElement('div')
                 contenerBkContener.classList.add('contener-bk-contener')
+                contenerBkContener.classList.add('tile')
                 divContener.appendChild(contenerBkContener)
     
                 const divBKContener = document.createElement('div')
@@ -27,17 +37,18 @@ const createDivBkContener = (bkTreeNodes, divContener) => {
     
                 const divBK = document.createElement('div')
                 divBK.classList.add('bk')
-                divBK.textContent = bkTreeNodesChild.title.substring(0, 10)
+                divBK.style.backgroundColor='var(--bg-color3)'
+                divBK.textContent = bkTreeNodesChild.title
                 divBKContener.appendChild(divBK)
     
                 const span = document.createElement('span')
                 span.classList.add('title')
-                span.textContent= bkTreeNodesChild.title.substring(0, 10)
+                span.textContent= bkTreeNodesChild.title.substring(0, 20)
                 contenerBkContener.appendChild(span)
             } else {
                 const divBK = document.createElement('div')
                 divBK.classList.add('bk')
-                divBK.textContent = bkTreeNodesChild.title.substring(0, 10)
+                divBK.textContent = bkTreeNodesChild.title
 
                 divContener.appendChild(divBK)
             }
